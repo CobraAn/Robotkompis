7d925b0cd6b204363e02698d2bda359cfaa7a40e
fredris <fredris@kth.se>
2016-04-18 22:29:02 +0200

    Start-Menu-TEST. First push!
    Set up some basic game states such as Boot, Preloader and StartMenu.
    Feel free to change the images, made them super fast :D

---
+++ www/src/states/StartMenu.js
@@ -0,0 +1,22 @@
+RobotKompis.StartMenu = function (game) {
+    'use strict';
+    this.startBG;
+    this.startPrompt;
+};
+
+RobotKompis.StartMenu.prototype = {
+    
+    create: function () {
+        'use strict';
+        this.startBG = this.add.image(0, 0, 'titleScreen');
+        this.startBG.inputEnabled = true;
+        this.startBG.events.onInputDown.addOnce(this.startGame, this);
+        
+        this.startPrompt = this.add.bitmapText(this.world.centerX - 190, this.world.centerY + 130, 'startFont', 'Touch to Start!', 50);
+    },
+    
+    startGame: function (pointer) {
+        'use strict';
+        this.state.start('Game');
+    }
+};
