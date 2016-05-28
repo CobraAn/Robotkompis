3c7c33b85afa51d88d7332df859f0334fb8e607f
cobraan <srbin1@live.se>
2016-05-28 20:49:16 +0200

    Fix

--- www/src/states/level.js
+++ www/src/states/level.js
@@ -1375,6 +1375,7 @@ RobotKompis.Level.prototype = {
         this.saveDataArgs.funcArray = saveFuncArray; // Put the fuction array as an argument to the this.saveDataArgs
         saveScore(noBlocks, this.saveDataArgs);
 
+        // Show WinScreen if the level is finished
         if (shouldSaveScore) {
             this.state.start('WinScreen', true, false, noBlocks, this.saveDataArgs);
         }
